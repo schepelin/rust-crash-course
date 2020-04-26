d0f5572376e530244e3048732695048c8334f498
Maxim Schepelin <m.schepelin@gmail.com>
2020-04-26 14:21:02 +0200

    task::spawn

--- usetokio/src/main.rs
+++ usetokio/src/main.rs
@@ -1,15 +1,32 @@
+use tokio::io;
+use tokio::task;
 use tokio::time;
-use tokio::process::Command;
-use std::time::Duration;
 
+use std::time::Duration;
+use tokio::process::Command;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error>{
-    let mut interval = time::interval(Duration::from_secs(1));
+    task::spawn(date()).await??;
+    task::spawn(intoout()).await??;
 
+    Ok(())
+}
+
+async fn intoout() -> io::Result<()> {
+
+    let mut input = io::stdin();
+    let mut stdout = io::stdout();
+
+    io::copy(&mut input, &mut stdout).await?;
+    Ok(())
+}
+
+
+async fn date() -> Result<(), std::io::Error> {
+    let mut interval = time::interval(Duration::from_secs(1));
     loop {
         interval.tick().await;
         Command::new("date").spawn()?.await?;
     }
-
 }
