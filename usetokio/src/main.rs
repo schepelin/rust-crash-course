b717ca6236357950a1bd93c7555e4bdb30629053
Maxim Schepelin <m.schepelin@gmail.com>
2020-04-26 13:52:06 +0200

    several awaits

--- usetokio/src/main.rs
+++ usetokio/src/main.rs
@@ -1,9 +1,15 @@
+use tokio::time;
 use tokio::process::Command;
+use std::time::Duration;
+
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error>{
-    for _ in 1..10 {
-        Command::new("echo").arg("Hello world").spawn()?.await?;
-    }
+    Command::new("date").spawn()?.await?;
+    time::delay_for(Duration::from_secs(1)).await;
+    Command::new("date").spawn()?.await?;
+    time::delay_for(Duration::from_secs(1)).await;
+    Command::new("date").spawn()?.await?;
+
     Ok(())
 }
